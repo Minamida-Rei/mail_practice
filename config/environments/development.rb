1b044a00a5698f0e33849a20466380e476400ebd
Minamida-Rei <rxnt1120@gmail.com>
2021-07-25 22:16:47 +0900

    サーバー設定

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,7 +32,17 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smpt_settings = {
+    port: 587,
+    address: 'smtp.gmail.com',
+    domain: 'gmail.com',
+    user_name: 'MyMailAddress@gmail.com'
+    password: 'MyPassword',
+    authentication: 'login',
+    enable_starttls_auto: true
+  }
 
   config.action_mailer.perform_caching = false
 
